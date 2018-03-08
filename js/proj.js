191e86bb8678d37af8d57df2d822689ca17f0cd9
jacktown11 <jikai_tang@163.com>
2018-03-08 22:54:12 +0800

    大量修改样式，项目页面改版核心工作完成

--- js/proj.js
+++ js/proj.js
@@ -1,20 +1,70 @@
-window.onload = function(){
-	$('li').click(function(){
-		loadNew($(this));
-	});
 
-	loadNew($('li[data-src="intro.html"]'));
-};
+;(function(){
+	var imagePath = '../images/proj/',
+		missImgSrc = imagePath + 'missing.jpg';
+		
+	window.onload = function () {
+		init();
+	};
 
-function loadNew($ele){
-	var $oldEle = $('li.active');
-	$oldEle.removeClass('active');
-	$oldEle.next().removeClass('down');
-	$oldEle.prev().removeClass('up');
+	function init(){
+		ajaxGet('../projects.json', renderProjects);
+	}
 
-	$ele.addClass('active');
-	$ele.next().addClass('down');
-	$ele.prev().addClass('up');
+	function renderProjects(jsonData){
+		var data = JSON.parse(jsonData);
+		var frag = document.createDocumentFragment();
+		data.forEach(function(item, index, arr){
+			var name = item.name,
+				show = item.show,
+				coverPath = imagePath + item.cover,
+				github = item.github;
+			frag.appendChild(createProjItem(name, show, coverPath, github));
+		});
+		var parent = document.getElementsByClassName('import-content')[0];
+		parent.appendChild(frag);
+	}
 
-	$('.content').load($ele.attr('data-src'));
-}
+	function ajaxGet(url, successCall){
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', url, true);
+		xhr.onreadystatechange = function(){
+			if(xhr.readyState === 4 && (xhr.status === 200 || xhr.status === 304)){
+				successCall.call(this, xhr.responseText);
+			}
+		};
+		xhr.send();
+	}
+
+	function createProjItem(name, show, coverPath, github){
+		var doc = document;
+		var div = doc.createElement('div');
+		div.className = 'proj';
+		div.setAttribute('data-show', show);
+
+		div.innerHTML = '<div class="links"><a href="' +
+			github + 
+			'"><img src="../images/github.png" alt="github图标"></a></div>' + 
+			'<img class="cover" src="' + 
+			coverPath + 
+			'" alt="项目封面"><h3>' + 
+			name + 
+			'</h3>';
+		return div;
+	}
+
+
+	/**
+	 * 加载一个新封面
+	 * @param {HTMLElement} imgEle 图片元素img 
+	 * @param {string} src 图片路径 
+	 */
+	function loadCover(imgEle, src){
+		var newImg = new Image();
+		newImg.src = src;
+		newImg.onload = function(event){
+			imgEle.src = src;
+		};
+	}	
+})();
+
