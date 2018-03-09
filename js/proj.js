0854f821171591dd6beb398dc9d5bb1fa4c7d44f
jacktown11 <jikai_tang@163.com>
2018-03-09 14:55:56 +0800

    项目页面改版完成

--- js/proj.js
+++ js/proj.js
@@ -1,4 +1,3 @@
-
 ;(function(){
 	var imagePath = '../images/proj/',
 		missImgSrc = imagePath + 'missing.jpg';
@@ -10,9 +9,25 @@
 	function init(){
 		ajaxGet('../projects.json', renderProjects);
 	}
+	function bindHandlers(){
+		var divs = document.getElementsByClassName('proj');
+		divs = [].slice.call(divs);
+		var handleClick = function(event){
+			if(event.target === this.firstChild){
+				return;
+			}else{
+				window.open(this.getAttribute('data-show'));
+				// console.log(this.getAttribute('data-show'));
+			}
+		};
+		divs.forEach(function(div, index, arr){
+			div.addEventListener('click',handleClick);
+		});
+	}
 
 	function renderProjects(jsonData){
 		var data = JSON.parse(jsonData);
+		// console.log(data);
 		var frag = document.createDocumentFragment();
 		data.forEach(function(item, index, arr){
 			var name = item.name,
@@ -23,17 +38,20 @@
 		});
 		var parent = document.getElementsByClassName('import-content')[0];
 		parent.appendChild(frag);
+		bindHandlers();
 	}
 
 	function ajaxGet(url, successCall){
 		var xhr = new XMLHttpRequest();
 		xhr.open('GET', url, true);
 		xhr.onreadystatechange = function(){
-			if(xhr.readyState === 4 && (xhr.status === 200 || xhr.status === 304)){
-				successCall.call(this, xhr.responseText);
+			if(xhr.readyState == 4){
+				if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
+					successCall.call(this, xhr.responseText);
+				}
 			}
 		};
-		xhr.send();
+		xhr.send(null);
 	}
 
 	function createProjItem(name, show, coverPath, github){
@@ -42,9 +60,9 @@
 		div.className = 'proj';
 		div.setAttribute('data-show', show);
 
-		div.innerHTML = '<div class="links"><a href="' +
+		div.innerHTML = '<a href="' +
 			github + 
-			'"><img src="../images/github.png" alt="github图标"></a></div>' + 
+			'" target="_blank"><img src="../images/github.png" alt="github图标"></a>' + 
 			'<img class="cover" src="' + 
 			coverPath + 
 			'" alt="项目封面"><h3>' + 
