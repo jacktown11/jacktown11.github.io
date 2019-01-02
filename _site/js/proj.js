f6b2c850f047e2b978dd252223a52eae3ac48ec6
jacktown11 <jikai_tang@163.com>
2019-01-02 21:41:32 +0800

    upgrade projects page

--- _site/js/proj.js
+++ _site/js/proj.js
@@ -1,71 +1,83 @@
-; (function () {
-	var imagePath = '../images/proj/';
 
-	window.onload = function () {
-		init();
-	};
+// ajax,
+// method: get and post
+// dataType: json, xml, txt
+function ajax(config) {
+	config = config || {};
+	config.type = config.type || 'GET';
+	config.async = config.async || 'true';
+	config.data = config.data || {};
+	config.success = config.success || function () { };
+	config.error = config.error || function () { };
 
-	function init() {
-		ajaxGet('../data/projects.json', renderProjects);
-	}
-
-	function renderProjects(jsonData) {
-		var data = JSON.parse(jsonData);
-		var frag = document.createDocumentFragment();
-		data.forEach(function (item, index, arr) {
-			var name = item.name,
-				show = item.show,
-				intro = item.intro,
-				coverPath = imagePath + item.cover,
-				github = item.github;
-			frag.appendChild(createProjItem(name, show, intro, coverPath, github));
-		});
-		document.body.appendChild(frag);
-	}
-
-	function ajaxGet(url, successCall) {
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', url, true);
-		xhr.onreadystatechange = function () {
-			if (xhr.readyState == 4) {
-				if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
-					successCall.call(this, xhr.responseText);
-				}
+	// 组装参数列表
+	let paramArr = [],
+		param = config.data;
+	for (let key in param) {
+		if (Array.isArray(param[key])) {
+			let arr = params[key];
+			for (let item of arr) {
+				paramArr.push(encodeURIComponent(key) + '=' + encodeURIComponent(item));
 			}
-		};
+		} else {
+			paramArr.push(encodeURIComponent(key) + '=' + encodeURIComponent(param[key]));
+		}
+	}
+	let paramStr = paramArr.join('&');
+
+	let xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4) {
+			if (xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
+				let data;
+				switch (config.dataType.toLowerCase()) {
+					case 'json':
+						if (window.JSON && JSON.parse) {
+							data = JSON.parse(xhr.responseText);
+						} else {
+							data = eval(xhr.responseText);
+						}
+						break;
+					case 'xml':
+						data = xhr.responseXML;
+						break;
+					default:
+						data = xhr.responseText;
+						break;
+				}
+				config.success(data);
+			} else {
+				config.error();
+			}
+		}
+	};
+	if (config.type.toUpperCase() === 'GET') {
+		xhr.open(config.type, config.url + '?' + paramStr, config.async);
 		xhr.send(null);
+	} else {
+		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		xhr.open(config.type, config.url, config.async);
+		xhr.send(paramStr);
 	}
-
-	function createProjItem(name, show, intro, coverPath, github) {
-		var doc = document;
-		var div = doc.createElement('div');
-		div.className = 'proj';
-
-		var githubIcon = '<a class="info github" href="' +
-			github +
-			'" target="_blank"><img src="../images/proj/github2.png" alt="github图标"></a>',
-			showIcon = !show ? '' :
-				('<a class="info show" href="' + show +
-					'" target="_blank"><img src = "../images/proj/show1.png" alt="在线演示"></a>'),
-			cover = '<img class="cover" src="' + coverPath +
-				'" alt="项目封面">',
-			detail = '<div class="detail"><div><h4>' + name + '</h4><p><span>' + intro + '<span></p></div></div>';
-		div.innerHTML = githubIcon + showIcon + cover + detail;
-		return div;
+}
+new Vue({
+	el: '#proj-list',
+	data: {
+		projects: [],
+		isLoaded: false
+	},
+	created: function () {
+		var self = this;
+		ajax({
+			url: '/data/projects.json',
+			dataType: 'json',
+			success: function (data) {
+				data.forEach(function (item) {
+					item.isEnlarge = false;
+				});
+				self.projects = data;
+				self.isLoaded = true;
+			}
+		});
 	}
-
-
-	/**
-	 * 加载一个新封面
-	 * @param {HTMLElement} imgEle 图片元素img 
-	 * @param {string} src 图片路径 
-	 */
-	function loadCover(imgEle, src) {
-		var newImg = new Image();
-		newImg.src = src;
-		newImg.onload = function (event) {
-			imgEle.src = src;
-		};
-	}
-})();
-
+});
